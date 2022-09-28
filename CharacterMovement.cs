d5e7fb73e97381d6de49d36def84277d122b3ca5
dylrlas <77942477+dylrlas@users.noreply.github.com>
2022-09-28 11:09:15 -0600

    Update CharacterMovement.cs

--- CharacterMovement.cs
+++ CharacterMovement.cs
@@ -11,39 +11,20 @@ public class CharacterMovement :  MonoBehaviour {
     private RigidBody2D theRB;
 
    
-    void Start() {
+    void Awake() {
 
       theRB = GetComponent<RigidBody2D>();
 
+       rigidBody2D.gravityScale = 0.0f;
     }
 
     void Update() {
 
-        moveSpeed.x = Input.GetAxisRow("Horizontal");
-        moveSpeed.y = Input.GetAxisRow("Vertical");
-
-       // transform.position = new Vector3(moveControl.x * Time.deltaTime * moveSpeed, moveControl.y * Time.deltaTime, 0f);
-
-       theRB.velocity = newVector2(horizontal * runSpeed, vertical * runSpeed);
-
-       Vector3 mousePos = Input.mousePosition;
-       Vector screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-
-       if(mousePos < screenPoint.x) {
-              transform.localScale = new Vector(-1f, 1f, 1f);
-       } //end if
-
-       else {
-         transform.localScale = Vector3.one;
-       }
-
-       if (moveSpeed != Vector2.zero) {
-          anim.setBool("isMoving", true);
-       }
-
-       else {
-          anim.setBook("isMoving", false);
-       } //end else
+       Vector2 catVelocity = new Vector2 (Input.getAxisRow("Horizontal"), Input.getAxisRow("Vertical"));
+       Move(catVelocity);
+        
+     void moveCharacter(Vector2 catVelocity) {
+         
+         theRB.velocity = (catVelocity * moveSpeed) * Time.deltaTIme;
    } //end Update
-   
 } //end CharacterMovement
