7ea77120ee8e8d765c1faad9ee5563fb6f937ed3
dylrlas <77942477+dylrlas@users.noreply.github.com>
2022-10-03 11:11:33 -0600

    Added character movement script

---
+++ Valse/Scripts/CharacterMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement :  MonoBehaviour {
+
+    public GameObject character;
+    
+    public float moveSpeed;
+    public float vertical;
+    public float runSpeed = 20.0f;
+
+    private RigidBody2D theRB;
+
+   
+    void Awake() {
+
+      theRB = GetComponent<RigidBody2D>();
+
+       rigidBody2D.gravityScale = 0.0f;
+    }
+
+    void Update() {
+        
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * runSpeed * time.deltaTime;
+        } //end if
+        
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * runSpeed * time.deltaTime;
+        } //end if
+        
+        if (Input.GetKeyCode(KeyCode.UpArrow)) {
+            transfrom.position += Vector3.forward * runSpeed * time.deltaTime;
+        } //end if
+        
+        if (Input.GetKeyCode(KeyCode.DownArrow)) {
+            transform.position += Vector3.back * runSpeed * time.deltaTime;
+        } //end if
+
+       Vector2 catVelocity = new Vector2 (Input.getAxisRow("Horizontal"), Input.getAxisRow("Vertical"));
+       moveCharacter(catVelocity);
+        
+     void moveCharacter(Vector2 catVelocity) {
+         theRB.velocity = (catVelocity * moveSpeed) * Time.deltaTime;
+   } //end Update
+} //end CharacterMovement class
